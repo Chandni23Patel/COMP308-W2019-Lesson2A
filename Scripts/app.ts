108b1b6edfd1fc568f8ab63e48dc15509059129b
chandni <chandptl.91@gmail.com>
2019-01-19 10:28:28 -0500

    changes in typescript

--- Scripts/app.ts
+++ Scripts/app.ts
@@ -1,11 +1,15 @@
-class Person{
+module objects {
+    export interface Greeter {
+        saysHello(): void;
+    }
 
-    //PRIVATE INSTANCE VARIABLE
+export abstract class Person implements Greeter {
+    // PRIVATE INSTANCE VARIABLES
     private _age: number;
     private _name: string;
 
-    //PUBLIC PROPERTIES
-    get age(): number{
+    // PUBLIC PROPERTIES
+    get age(): number {
         return this._age;
     }
 
@@ -13,7 +17,7 @@ class Person{
         this._age = newAge;
     }
 
-    get name(): string{
+    get name(): string {
         return this._name;
     }
 
@@ -21,22 +25,63 @@ class Person{
         this._name = newName;
     }
 
-    //constructore
-    constructor(age: number, name:string ) {
-        this._age = age;
-        this._name = name;
+    constructor(age: number, name:string) {
+        this.age = age;
+        this.name = name;
     }
 
     public saysHello():void {
-        console.log(`${this.name} says Hello..!!`);
+        console.log(`${this.name} says Hello!`);
     }
+    }
+
+    export class Student extends objects.Person {
+        // PRIVATE INSTANCE VARIABLES
+        private _studentID: string;
+
+        // PUBLIC PROPERTIES
+        get studentID(): string {
+            return this._studentID;
+        }
+
+        set studentID(newStudentID: string) {
+            this._studentID = newStudentID;
+        }
+
+        constructor(age:number, name:string, studentID:string) {
+            super(age, name);
+
+            this.studentID = studentID;
+        }
+
+        // PRIVATE METHODS
+
+        // PUBLIC METHODS
+        public studies(): void {
+            console.log(`${this.name} with studentID: ${this.studentID} is studying.`)
+        }
+    }
+
 }
 
-//IIFE --Immediately Invockeed Funcation Expression
 
-(funcation(){
-    funcation Start(){
-        let person = new Person(27, "chandni");
-        person.saysHello();
+/*
+const person: objects.Person = new objects.Person(30, "Tom");
+person.saysHello();
+*/
+
+// IIFE -- Immediately Invoked Function Expression
+(function(){
+
+    function Start() {
+        const student: objects.Student = new objects.Student(20, "Sarah", "P008490000");
+        student.saysHello();
+        student.studies();
     }
+
+    window.addEventListener("load", Start);
+
 })();
+
+
+
